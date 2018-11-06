8ca91bcfe7b523839e8033232fc2d38ac65ffe7a
leonardopaiva <leonardopaiva2010@gmail.com>
2018-11-06 17:56:08 -0300

    added user from github list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { DataService } from './data-service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'angular-universal-site';
+  users = [];
+
+  constructor(private dataService: DataService) { }
+
+  ngOnInit(): void {
+    console.log('teste');
+    this.getUsers();
+  }
+
+  getUsers() {
+    const usersObservable = this.dataService.getUsers();
+    console.log('calling subscribe...');
+    usersObservable.subscribe(
+      (data) => {
+        // console.log(data);
+        console.log('Data complete...');
+        this.users = data;
+      },
+      err => {
+        console.log('Erro: ', err);
+      },
+      () => {
+        console.log('Subscribe complete...');
+      }
+    );
+  }
+
 }
