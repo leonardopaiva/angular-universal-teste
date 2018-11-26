e45e3ef935e57451874df7b6c23ee92b46d78192
leonardopaiva <leonardopaiva2010@gmail.com>
2018-11-26 18:55:42 -0300

    #Feature Accordion Base

--- src/app/pages/demo/demo.component.ts
+++ src/app/pages/demo/demo.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AccordionContent } from 'src/app/shared/components/accordion/accordion-content.interface';
 
 @Component({
   selector: 'app-demo',
@@ -6,6 +7,37 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./demo.component.css']
 })
 export class DemoComponent implements OnInit {
+
+  accordionContent: AccordionContent[] = [{
+    title: 'Curso 001',
+    link: 'link01',
+    smallDescription: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptates accusantium fugiat reprehenderit necessitatibus eum provident veniam
+      vitae laboriosam expedita ipsa nostrum temporibus possimus, consequuntur quis
+      magnam! Numquam molestias illo a.
+      quebra de linha asidjasi djasidj iasjdi ajsidjs aijdiajsij
+    `,
+    description: `Conteúdo escondido... Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptates accusantium fugiat reprehenderit necessitatibus eum provident veniam
+      vitae laboriosam expedita ipsa nostrum temporibus possimus, consequuntur quis
+      magnam! Numquam molestias illo a.
+    `
+  }, {
+    title: 'Curso 002',
+    link: 'link02',
+    smallDescription: `Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptates accusantium fugiat reprehenderit necessitatibus eum provident veniam
+      vitae laboriosam expedita ipsa nostrum temporibus possimus, consequuntur quis
+      magnam! Numquam molestias illo a.
+      quebra de linha asidjasi djasidj iasjdi ajsidjs aijdiajsij
+    `,
+    description: `Conteúdo escondido... Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+      Voluptates accusantium fugiat reprehenderit necessitatibus eum provident veniam
+      vitae laboriosam expedita ipsa nostrum temporibus possimus, consequuntur quis
+      magnam! Numquam molestias illo a.
+    `
+  }
+];
 
   constructor() { }
 
---
+++ src/app/shared/components/accordion/accordion-content.interface.ts
@@ -0,0 +1,6 @@
+export interface AccordionContent {
+  title: string;
+  link: string;
+  smallDescription: string;
+  description: string;
+}
